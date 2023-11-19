5b00530d47bea88b57f20c3aea63e940de728941
Pujan Gautam <mepujan10@gmail.com>
2023-11-18 21:27:16 -0500

    adding validation to RatingSerializers to be in range 1 to 5

--- product/serializers.py
+++ product/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Category, Product, Rating
+from django.core.exceptions import ValidationError
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -22,3 +23,9 @@ class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = "__all__"
+
+    def validate_rating(self, rating):
+        if rating not in range(1, 6):
+            raise ValidationError(
+                "Rating Should be in range 1 to 5 inclusive.")
+        return rating
