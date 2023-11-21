5af27732870d730aea5acfa4c4edddfb55d4f83f
Pujan Gautam <mepujan10@gmail.com>
2023-11-20 19:25:11 -0500

    updated admin panel site name, title

--- product/admin.py
+++ product/admin.py
@@ -4,3 +4,8 @@ from .models import Category, Product, Rating
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Rating)
+
+
+admin.site.site_header = "Pujan's Shop"
+admin.site.site_title = "Pujan's Shop Admin Portal"
+admin.site.index_title = "Welcome to Pujan's Shop  Portal"
