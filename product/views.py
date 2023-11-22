3c00779fd933b6a3e7680fdb66b189b79a773e0b
Pujan Gautam <mepujan10@gmail.com>
2023-11-22 00:54:05 -0500

    product filter and search using django-filters

--- product/views.py
+++ product/views.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Category, Product, Rating
 from .serializers import CategorySerializer, ProductSerializer, RatingSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import ProductFilterClass
+from rest_framework.filters import SearchFilter
 
 
 class CategoryModelViewSet(ModelViewSet):
@@ -11,6 +14,10 @@ class CategoryModelViewSet(ModelViewSet):
 class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['name']
+    filterset_fields = ['category', 'name', 'stock', 'price']
+    filterset_class = ProductFilterClass
 
 
 class RatingModelViewSet(ModelViewSet):
