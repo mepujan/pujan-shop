40e559f7d332a602c1610c830950ece27e0dd435
Pujan Gautam <mepujan10@gmail.com>
2023-11-24 15:30:12 -0500

    create token and cart when user created

--- cart/signals.py
+++ cart/signals.py
@@ -2,9 +2,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Cart
+from rest_framework.authtoken.models import Token
 
 
 @receiver(post_save, sender=User)
 def create_cart(sender, instance, created, **kwargs):
     if created:
         Cart.objects.create(user=instance)
+        Token.objects.create(user=instance)
