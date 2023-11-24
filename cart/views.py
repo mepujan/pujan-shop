19f931785d856aeb45602151543e53863d1076df
Pujan Gautam <mepujan10@gmail.com>
2023-11-23 22:35:58 -0500

    added exception handling when fetching cart

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import CartItem, Cart
 from .serializers import CartItemSerializer, CartSerializer, AddtoCartSerializer, UpdateCartSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class CartItemModelViewSet(ModelViewSet):
@@ -10,8 +12,22 @@ class CartItemModelViewSet(ModelViewSet):
         qs = CartItem.objects.filter(user=self.request.user)
         return qs
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        except Cart.DoesNotExist:
+            return Response({'error': 'User doesnot exist'}, status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response({'error': 'Something went wrong'}, status=status.HTTP_404_NOT_FOUND)
+
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+
+        cart = Cart.objects.get(user=self.request.user)
+        serializer.save(user=self.request.user, cart=cart)
 
     def get_serializer_class(self):
         if self.request.method == "POST":
