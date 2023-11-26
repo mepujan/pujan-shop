0640da71744fedef80959a79c11d07a4219a431f
Pujan Gautam <mepujan10@gmail.com>
2023-11-25 23:23:39 -0500

    override get serializer class method for order viewset

--- order/views.py
+++ order/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import OrderItemSerializer, OrderSerializer, CreateOrderSerializer
+from .models import Order, OrderItem
 
-# Create your views here.
+
+class OrderItemViewSet(ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        return OrderSerializer
+
+    def get_serializer_context(self):
+        return {'user': self.request.user}
